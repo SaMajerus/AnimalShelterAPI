08d510538669c30c7be44be37266b47c478faab7
Samuel Majerus <ladolego@gmail.com>
2022-10-28 14:37:17 -0700

    Fixed 'LaunchURL' routings in 'launchSettings.json'.   Adds migration 'SeedData'. [WIP:  Test with Postman.    Look into implementing Swagger documentation (already have the 'Swashbuckle' dependency in the csproj file).]

--- AnimalShelter/Migrations/20221028213401_SeedData.Designer.cs
+++ AnimalShelter/Migrations/20221028213401_SeedData.Designer.cs
@@ -2,14 +2,16 @@
 using AnimalShelter.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnimalShelter.Migrations
 {
     [DbContext(typeof(AnimalShelterContext))]
-    partial class AnimalShelterContextModelSnapshot : ModelSnapshot
+    [Migration("20221028213401_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AnimalShelter/Migrations/20221028213401_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, 3, null, "Victoria", "Dog" },
+                    { 2, 5, null, "Zeus", "Dog" },
+                    { 3, 4, null, "Remmie", "Dog" },
+                    { 4, 1, null, "Todd", "Cat" },
+                    { 5, 2, null, "Buffy", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
