93af1247562dd306213f8e0b616897b2c6b8b7f5
Samuel Majerus <ladolego@gmail.com>
2022-10-28 15:36:24 -0700

    Adjusts AnimalId values for some of the 'new' object declarations

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -7,7 +7,7 @@ namespace AnimalShelter.Models
         public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
             : base(options)
         {
-        } /*
+        } 
         protected override void OnModelCreating(ModelBuilder builder)
         {
           builder.Entity<Animal>()
@@ -15,10 +15,10 @@ namespace AnimalShelter.Models
                   new Animal { AnimalId = 1, Name = "Victoria", Type = "Dog", Age = 3 },
                   new Animal { AnimalId = 2, Name = "Zeus", Type = "Dog", Age = 5 },
                   new Animal { AnimalId = 3, Name = "Remmie", Type = "Dog", Age = 4 }, 
-                  new Animal { AnimalId = 1, Name = "Todd", Type = "Cat", Age = 1 },
-                  new Animal { AnimalId = 2, Name = "Buffy", Type = "Cat", Age = 2 }
+                  new Animal { AnimalId = 4, Name = "Todd", Type = "Cat", Age = 1 },
+                  new Animal { AnimalId = 5, Name = "Buffy", Type = "Cat", Age = 2 }
               );
-        }  */ 
+        } 
         public DbSet<Animal> Animals { get; set; } 
     }
 }
